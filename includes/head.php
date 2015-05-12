5084e92631605f543a904878e353f9546891b2ae
Alexandr <maler1988@yandex.ru>
2015-05-12 23:28:28 +0300

    Вёрстка верхнее меню

---
+++ includes/head.php
@@ -0,0 +1,20 @@
+<head>
+	<title>Верстка ВГИК</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,600,600italic,400italic,700,700italic,800,800italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css" />
+</head>	
+<header class="header-block" >	
+	<img src="images/logo.png" >
+	<span>Ростовский-на-Дону филиал всероссийского государственного университета кинематографии имени С.А. Герасимова</span>
+</header>
+<nav class="top-menu" >
+	<ul class="menu" >
+		<li><span><a href="" >Сведения о филиале</a></span></li>
+		<li><span><a href="" >Абитурьентам</a></span></li>
+		<li><span><a href="" >Студентам</a></span></li>
+		<li><span><a href="" >Преподавателям</a></span></li>
+		<li><span><a href="" >Контакты</a></span></li>
+	</ul>
+	<div class="clearfix" ></div>
+</nav>
